6679350c6e2c3e319586d8ec5dc741d9ee1dc77d
AngellaPrincy <angellaprincy2013@gmail.com>
2026-04-08 22:11:00 +0530

    fix:change the year

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
         {/* Footer */}
         <footer className="bg-gray-900 text-gray-300 py-6 px-4 mt-12">
           <div className="max-w-7xl mx-auto text-center">
-            <p>&copy; 2024 Inventory & Stock Management System. All rights reserved.</p>
+            <p>&copy; 2026 Inventory & Stock Management System. All rights reserved.</p>
           </div>
         </footer>
 
